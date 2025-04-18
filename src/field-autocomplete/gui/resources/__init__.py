dc2b87aa883e0be25afa0c211bc027949cd61e0c
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-18 17:39:33 +0100

    feat: Add GUI files and resources for field autocomplete

---
+++ src/field-autocomplete/gui/resources/__init__.py
@@ -0,0 +1,13 @@
+# This file makes the resources directory a Python package
+# and provides functions to initialize Qt resources
+
+import os
+
+def initialize_qt_resources():
+    """
+    Initialize Qt resources for the add-on.
+
+    This is a placeholder function that doesn't actually need to do anything
+    since we're loading resources directly from files in the settings_dialog.py.
+    """
+    return True
